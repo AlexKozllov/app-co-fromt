27141e5dc564a2bf5b489163a6786dcc3a30552c
AlexKozllov <62513252+AlexKozllov@users.noreply.github.com>
2022-11-13 23:41:06 +0200

    Delete build directory

--- build/static/js/5.174bd987.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpreact-template"]=this["webpackJsonpreact-template"]||[]).push([[5],{62:function(t,a,s){t.exports={navigation:"mainPage_navigation__CMLR3",userStatistics:"mainPage_userStatistics__QTZMM"}},87:function(t,a,s){"use strict";s.r(a);s(0);var i=s(19),e=s(62),n=s.n(e),c=s(1);a.default=function(){return Object(c.jsx)("div",{children:Object(c.jsx)("nav",{className:n.a.navigation,children:Object(c.jsx)(i.b,{to:"/userStatistics",className:n.a.userStatistics,children:"Go to UserStatistics"})})})}}}]);
-//# sourceMappingURL=5.174bd987.chunk.js.map
